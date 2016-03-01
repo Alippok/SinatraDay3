2ba48fcfcbe38a0302894df4f650954cd217e915
A_Lippok <alippok@hotmail.co.uk>
2016-03-01 19:33:00 +0000

    Adds methods to display content

--- controller.rb
+++ controller.rb
@@ -1,10 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('../models/bio.rb')
+require_relative('./models/bio.rb')
 require('json')
 
 
-get '/' do
-
-
+get '/' do 
+  bio = Bio.new(name: "Alasdair Lippok", phone: "0131558030", address_first_line: "3 Argyle house", postcode: "eh13 zqf", location: "codebase")
+  @script = bio.welcome_script
+  @name = bio.name
+  erb(:bio_page)
 end
