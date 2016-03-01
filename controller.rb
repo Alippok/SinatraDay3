8e064dcfa2e3dde09da39d6790c9be5df91b4f00
A_Lippok <alippok@hotmail.co.uk>
2016-03-01 20:11:38 +0000

    Adds styling to mainpage and contact details in json

--- controller.rb
+++ controller.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./models/bio.rb')
+require_relative('./models/wordformatter.rb')
 require('json')
 
 
@@ -9,4 +10,23 @@ get '/' do
   @script = bio.welcome_script
   @name = bio.name
   erb(:bio_page)
+end
+
+get '/address' do
+  content_type(:json)
+  bio = Bio.new(name: "Alasdair Lippok", phone: "0131558030", address_first_line: "3 Argyle house", postcode: "eh13 zqf", location: "codebase")
+  wordformatter = Wordformatter.new
+  
+  altered_address = wordformatter.upcase(bio.address_first_line)
+  altered_postcode = wordformatter.upcase(bio.postcode)
+  altered_location = wordformatter.upcase(bio.location)
+    result = {
+    address_first_line: altered_address,
+    building: altered_location,
+    postcode: altered_postcode,
+    phone: bio.phone,
+  }
+
+  return result.to_json
+  
 end
